c07f99ecafcbc9f9d33a2973f91d66d2ecc3524e
sunmaer <878131732@qq.com>
2017-08-13 22:50:57 +0800

    D3 绘制树状图

--- src/page/chart/D3graph.vue
+++ src/page/chart/D3graph.vue
@@ -1,7 +1,6 @@
 <template>
   <el-row>
-    <!--<div id='main' style='width: 100% height: 500px'></div>-->
-    <svg width="100%" height="500"></svg>
+    <svg width="1100" height="500"></svg>
   </el-row>
 </template>
 
@@ -13,67 +12,59 @@ export default {
   methods: {
     /* 绘制图表 */
     drawChart() {
-      let svg = this.$d3.select('svg')
-      let width = +svg.attr('width')
-      let height = +svg.attr('height')
-      let g = svg.append('g').attr('transform', 'translate(40,0)')
+      let svg = this.$d3.select("svg")
+      let width = +svg.attr("width")
+      let height = +svg.attr("height")
+      let g = svg.append("g").attr("transform", "translate(100,0)")
 
       let tree = this.$d3.tree()
-        .size([height - 400, width - 160])
-
-      let cluster = this.$d3.cluster()
         .size([height, width - 160])
-        
-      this.$d3.json('static/data.json', function (error, root) {
+
+      this.$d3.json("static/data.json", (error, data) => {
         if (error) throw error
 
-        cluster(root)
+        let root = this.$d3.hierarchy(data)
+        tree(root)
 
-        let link = g.selectAll('.link')
+        let link = g.selectAll(".link")
           .data(root.descendants().slice(1))
-          .enter().append('path')
-          .attr('class', 'link')
-          .attr('d', diagonal)
+          .enter().append("path")
+          .attr("class", "link")
+          .attr("d", function (d) {
+            return "M" + d.y + "," + d.x
+              + "C" + (d.parent.y + 120) + "," + d.x
+              + " " + (d.parent.y + 120) + "," + d.parent.x
+              + " " + d.parent.y + "," + d.parent.x
+          })
+          // .attr("d", function(d){
+          //       return "M"+d.y+" "+d.x+
+          //       "L"+(d.parent.y+120)+" "+d.x+
+          //       " L"+(d.parent.y+120)+" "+d.parent.x+" L"+
+          //       d.parent.y+" "+d.parent.x
+          // })
+          // .attr("style", function () {
+          //   return "stroke:#F7881F"
+          // });
 
-        let node = g.selectAll('.node')
+        let node = g.selectAll(".node")
           .data(root.descendants())
-          .enter().append('g')
-          .attr('class', function (d) { return 'node' + (d.children ? ' node--internal' : ' node--leaf') })
-          .attr('transform', function (d) { return 'translate(' + d.y + ',' + d.x + ')' })
+          .enter().append("g")
+          .attr("class", function (d) { return "node" + (d.children ? " node--internal" : " node--leaf") })
+          .attr("transform", function (d) {
+            return "translate(" + d.y + "," + d.x + ")"
+          })
 
-        node.append('circle')
-          .attr('r', 2.5)
+        node.append("circle")
+          .attr("r", 2.5);
 
-        node.append('text')
-          .attr('dy', 3)
-          .attr('x', function (d) { return d.children ? -8 : 8 })
-          .style('text-anchor', function (d) { return d.children ? 'end' : 'start' })
-          .text(function (d) { return d.id.substring(d.id.lastIndexOf('.') + 1) })
-
-        this.$d3.selectAll('input')
-          .on('change', changed)
-
-        let timeout = setTimeout(function () {
-          this.$d3.select('input[value=\'tree\']')
-            .property('checked', true)
-            .dispatch('change')
-        }, 1000)
-
-        function changed() {
-          timeout = clearTimeout(timeout)
-            (this.value === 'tree' ? tree : cluster)(root)
-          let t = this.$d3.transition().duration(750)
-          node.transition(t).attr('transform', function (d) { return 'translate(' + d.y + ',' + d.x + ')' })
-          link.transition(t).attr('d', diagonal)
-        }
+        node.append("text")
+          .attr("dy", 3)
+          .attr("x", function (d) { return d.children ? -8 : 8; })
+          .style("text-anchor", function (d) { return d.children ? "end" : "start" })
+          .text(function (d) {
+            return d.data.name
+          })
       })
-
-      function diagonal(d) {
-        return 'M' + d.y + ',' + d.x
-          + 'C' + (d.parent.y + 100) + ',' + d.x
-          + ' ' + (d.parent.y + 100) + ',' + d.parent.x
-          + ' ' + d.parent.y + ',' + d.parent.x
-      }
     }
   },
   mounted() {
@@ -83,22 +74,13 @@ export default {
 </script>
 
 <style scoped>
+  .node {
+    font: 12px sans-serif;
+  }
+
   .link {
     fill: none;
-    stroke: #aaa;
-  }
-
-  .node text {
-    font: 10px sans-serif;
-  }
-
-  .node circle {
-    stroke: #fff;
+    stroke: #ccc;
     stroke-width: 1.5px;
   }
-
-  .node.active {
-    fill: red;
-  }
 </style>
-
