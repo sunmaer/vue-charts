c0a75cd1af2e3b892cdfc6fb8df0bb675db5e3f6
sunmenghai <sunmenghai@b.360.cn>
2017-08-15 14:45:30 +0800

    通用组件

--- src/page/chart/Graph.vue
+++ src/page/chart/Graph.vue
@@ -1,139 +1,33 @@
 <template>
-  <el-row>
-    <div id="main" style="width: 100%; height: 500px"></div>
-  </el-row>
+  <div>
+    <div id="graph" style="width: 100%; height: 500px"></div>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
-      return {
-        nodes: [
-          {
-            name: 'node1'
-          },
-          {
-            name: 'node2'
-          },
-          {
-            name: 'node3'
-          },
-          {
-            name: 'node4'
-          },
-          {
-            name: 'node5'
-          },
-          {
-            name: 'node6'
-          },
-          {
-            name: 'node7'
-          },
-          {
-            name: 'node8'
-          },
-          {
-            name: 'node9'
-          },
-          {
-            name: 'node10'
-          },
-          {
-            name: 'node11'
-          },
-          {
-            name: 'node12'
-          },
-          {
-            name: 'node13'
-          },
-          {
-            name: 'node14'
-          },
-          {
-            name: 'node15'
-          }
-        ],
-        links: [
-          {
-            source: 'node1',
-            target: 'node2',
-            value: 100
-          },
-          {
-            source: 'node1',
-            target: 'node3',
-            value: 200
-          },
-          {
-            source: 'node1',
-            target: 'node4',
-            value: 300
-          },
-          {
-            source: 'node1',
-            target: 'node5',
-            value: 400
-          },
-          {
-            source: 'node1',
-            target: 'node6',
-            value: 500
-          },
-          {
-            source: 'node1',
-            target: 'node7',
-            value: 100
-          },
-          {
-            source: 'node1',
-            target: 'node8',
-            value: 200
-          },
-          {
-            source: 'node1',
-            target: 'node9',
-            value: 300
-          },
-          {
-            source: 'node1',
-            target: 'node10',
-            value: 400
-          },
-          {
-            source: 'node1',
-            target: 'node11',
-            value: 500
-          },
-          {
-            source: 'node1',
-            target: 'node12',
-            value: 100
-          },
-          {
-            source: 'node1',
-            target: 'node13',
-            value: 200
-          },
-          {
-            source: 'node1',
-            target: 'node14',
-            value: 300
-          },
-          {
-            source: 'node1',
-            target: 'node15',
-            value: 400
-          }
-        ]
+      return {}
+    },
+    props: {
+      nodes: {
+        type: Array,
+        default () {
+          return []
+        }
+      },
+      links: {
+        type: Array,
+        default () {
+          return []
+        }
       }
     },
     methods: {
       /* 绘制图表 */
-      drawChart () {
+      render () {
         // 初始化 Echarts 实例
-        let myChart = this.$echarts.init(document.getElementById('main'))
+        let myChart = this.$echarts.init(document.getElementById('graph'))
 
         // 指定图表的配置项和数据
         let option = {
@@ -188,7 +82,7 @@
       }
     },
     mounted () {
-      this.drawChart()
+      this.render()
     }
   }
 </script>
---
+++ src/page/chart/Test.vue
@@ -0,0 +1,140 @@
+<template>
+  <Test :nodes="nodes" :links="links"></Test>
+</template>
+
+<script>
+  import Test from './Graph.vue'
+
+  export default {
+    data () {
+      return {
+        nodes: [
+          {
+            name: 'node1'
+          },
+          {
+            name: 'node2'
+          },
+          {
+            name: 'node3'
+          },
+          {
+            name: 'node4'
+          },
+          {
+            name: 'node5'
+          },
+          {
+            name: 'node6'
+          },
+          {
+            name: 'node7'
+          },
+          {
+            name: 'node8'
+          },
+          {
+            name: 'node9'
+          },
+          {
+            name: 'node10'
+          },
+          {
+            name: 'node11'
+          },
+          {
+            name: 'node12'
+          },
+          {
+            name: 'node13'
+          },
+          {
+            name: 'node14'
+          },
+          {
+            name: 'node15'
+          }
+        ],
+        links: [
+          {
+            source: 'node1',
+            target: 'node2',
+            value: 100
+          },
+          {
+            source: 'node1',
+            target: 'node3',
+            value: 200
+          },
+          {
+            source: 'node1',
+            target: 'node4',
+            value: 300
+          },
+          {
+            source: 'node1',
+            target: 'node5',
+            value: 400
+          },
+          {
+            source: 'node1',
+            target: 'node6',
+            value: 500
+          },
+          {
+            source: 'node1',
+            target: 'node7',
+            value: 100
+          },
+          {
+            source: 'node1',
+            target: 'node8',
+            value: 200
+          },
+          {
+            source: 'node1',
+            target: 'node9',
+            value: 300
+          },
+          {
+            source: 'node1',
+            target: 'node10',
+            value: 400
+          },
+          {
+            source: 'node1',
+            target: 'node11',
+            value: 500
+          },
+          {
+            source: 'node1',
+            target: 'node12',
+            value: 100
+          },
+          {
+            source: 'node1',
+            target: 'node13',
+            value: 200
+          },
+          {
+            source: 'node1',
+            target: 'node14',
+            value: 300
+          },
+          {
+            source: 'node1',
+            target: 'node15',
+            value: 400
+          }
+        ]
+      }
+    },
+    components: {
+      Test
+    }
+  }
+</script>
+
+<style>
+
+</style>
